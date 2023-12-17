a21016de55f41d0464793fffa0ef707585edf69d
odyshkantiuk <odyshkantiuk@gmail.com>
2023-12-17 20:21:28 +0200

    Add schemas.py and config.py

---
+++ myapplication/config.py
@@ -0,0 +1,12 @@
+PROPAGATE_EXCEPTIONS = True
+FLASK_DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost:5432/flaskdb"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+API_TITLE = "Finance REST API"
+API_VERSION = "v1"
+OPENAPI_VERSION = "3.0.3"
+OPENAPI_URL_PREFIX = "/"
+OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
