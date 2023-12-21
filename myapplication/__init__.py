493d2270d21e0697e3ca268321a5fa641b33c8c8
odyshkantiuk <odyshkantiuk@gmail.com>
2023-12-21 15:14:16 +0200

    Delete updater

--- myapplication/__init__.py
+++ myapplication/__init__.py
@@ -4,27 +4,6 @@ from flask_migrate import Migrate
 import os
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-import psycopg2
-
-
-def get_db_connection():
-    return psycopg2.connect(host="dpg-clvkga6d3nmc738e26ag-a", database="flaskdb_7uea", user="admin", password="yZqV5iPhvzOI9gPEVWVOfpV2zGm0DMXJ")
-
-
-def create_tables():
-    conn = None
-    try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute("""ALTER TABLE users
-        ADD COLUMN password VARCHAR(255) NOT NULL DEFAULT 'default_password';""")
-        cur.close()
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
 
 
 app = Flask(__name__)
@@ -35,7 +14,6 @@ db = SQLAlchemy(app)
 api = Api(app)
 jwt = JWTManager(app)
 migrate = Migrate(app, db)
-create_tables()
 
 import myapplication.views
 import myapplication.models
