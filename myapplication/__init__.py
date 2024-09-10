6d881d68c2db192f3f55f3f98f50db1f1888e269
odyshkantiuk <odyshkantiuk@gmail.com>
2024-09-10 18:36:23 +0300

    Update db

--- myapplication/__init__.py
+++ myapplication/__init__.py
@@ -12,12 +12,44 @@ def get_db_connection():
 
 
 def create_tables():
+    commands = (
+        """ 
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR (20) NOT NULL
+            password VARCHAR(255) NOT NULL DEFAULT 'default_password'
+        );
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS accounts (
+            id SERIAL PRIMARY KEY,
+            user_id INT,
+            money INT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        );
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS categories ( 
+            id SERIAL PRIMARY KEY, 
+            name VARCHAR(20) NOT NULL 
+        );
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS records ( 
+            id SERIAL PRIMARY KEY,
+            user_id INT,
+            category_id INT, 
+            time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            amount_of_expenditure FLOAT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users (id),
+            FOREIGN KEY (category_id) REFERENCES categories (id)
+        );""")
     conn = None
     try:
         conn = get_db_connection()
         cur = conn.cursor()
-        cur.execute("""ALTER TABLE users
-        ADD COLUMN password VARCHAR(255) NOT NULL DEFAULT 'default_password';""")
+        for command in commands:
+            cur.execute(command)
         cur.close()
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -25,7 +57,6 @@ def create_tables():
     finally:
         if conn is not None:
             conn.close()
-
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
