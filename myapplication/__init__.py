a0448408409bf829b14885c4882d11006276011c
odyshkantiuk <odyshkantiuk@gmail.com>
2024-09-10 18:42:31 +0300

    ops

--- myapplication/__init__.py
+++ myapplication/__init__.py
@@ -16,7 +16,7 @@ def create_tables():
         """ 
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
-            name VARCHAR (20) NOT NULL
+            name VARCHAR (20) NOT NULL,
             password VARCHAR(255) NOT NULL DEFAULT 'default_password'
         );
         """,
